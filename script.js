8c52494d56e968eb630dea1cc687c81717be9871
shivdasbhande05 <shivdasbhande@gmail.com>
2025-02-02 21:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+    let strongWords = document.querySelectorAll("strong");
+    strongWords.forEach(word => {
+        word.style.color = "green";
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    let strongWords = document.querySelectorAll("strong");
+    strongWords.forEach(word => {
+        word.style.color = "";
+    });
     
 }
